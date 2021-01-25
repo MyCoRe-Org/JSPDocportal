c84bbf05c21ea0061489e7c95c2cf4c7754d123a
Robert Stephan <robert.stephan@uni-rostock.de>
2021-01-25 18:08:31 +0100

    switch workflow engine - rename classes

--- jspdocportal-ir-module/src/main/java/org/mycore/jspdocportal/ir/bpmn/workflows/create_object_simple/MCRWorkflowMgrThesis.java
+++ jspdocportal-ir-module/src/main/java/org/mycore/jspdocportal/ir/bpmn/workflows/create_object_simple/MCRWorkflowMgrThesis.java
@@ -6,13 +6,13 @@ import java.nio.file.Path;
 
 import org.jdom2.Document;
 import org.jdom2.input.SAXBuilder;
-import org.mycore.activiti.workflows.create_object_simple.MCRAbstractWorkflowMgr;
-import org.mycore.activiti.workflows.create_object_simple.MCRWorkflowMgr;
 import org.mycore.common.MCRException;
 import org.mycore.datamodel.metadata.MCRObject;
 import org.mycore.datamodel.metadata.MCRObjectID;
 import org.mycore.datamodel.metadata.MCRObjectMetadata;
 import org.mycore.jspdocportal.common.bpmn.MCRBPMNUtils;
+import org.mycore.jspdocportal.common.bpmn.workflows.create_object_simple.MCRAbstractWorkflowMgr;
+import org.mycore.jspdocportal.common.bpmn.workflows.create_object_simple.MCRWorkflowMgr;
 import org.xml.sax.SAXParseException;
 
 public class MCRWorkflowMgrThesis extends MCRAbstractWorkflowMgr implements MCRWorkflowMgr {
