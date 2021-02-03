c19e080ea9d5d23168036a71e3d176fe041f6ef0
Robert Stephan <robert.stephan@uni-rostock.de>
2021-02-03 17:07:00 +0100

    move MCREditorMetadatValidator4MCRMetaXML to new package

--- jspdocportal-common-module/src/main/java/org/mycore/jspdocportal/common/editor/MCREditorMetadatValidator4MCRMetaXML.java
+++ jspdocportal-common-module/src/main/java/org/mycore/jspdocportal/common/editor/MCREditorMetadatValidator4MCRMetaXML.java
@@ -19,7 +19,7 @@
  * If not, write to the Free Software Foundation Inc.,
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
  */
-package org.mycore.frontend.editor;
+package org.mycore.jspdocportal.common.editor;
 
 import java.util.ArrayList;
 import java.util.List;
