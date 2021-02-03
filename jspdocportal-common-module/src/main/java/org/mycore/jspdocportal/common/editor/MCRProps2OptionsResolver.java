a300ebef25cef20065e2645530ae7a7cd2053cc5
Robert Stephan <robert.stephan@uni-rostock.de>
2021-02-03 17:05:09 +0100

    move MCRProps2OptionsResolver to new package

--- jspdocportal-common-module/src/main/java/org/mycore/jspdocportal/common/editor/MCRProps2OptionsResolver.java
+++ jspdocportal-common-module/src/main/java/org/mycore/jspdocportal/common/editor/MCRProps2OptionsResolver.java
@@ -21,7 +21,7 @@
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
  */
 
-package org.mycore.frontend.editor;
+package org.mycore.jspdocportal.common.editor;
 
 import javax.xml.transform.Source;
 import javax.xml.transform.TransformerException;
