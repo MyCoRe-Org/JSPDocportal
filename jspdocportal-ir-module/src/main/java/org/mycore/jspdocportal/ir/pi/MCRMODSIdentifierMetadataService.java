425d4ff1263fcaa5fceffabf65c22266bf3ca804
Robert Stephan <robert.stephan@uni-rostock.de>
2021-06-24 18:31:15 +0200

    add new implementation for MODSIdentifierMetadataService

---
+++ jspdocportal-ir-module/src/main/java/org/mycore/jspdocportal/ir/pi/MCRMODSIdentifierMetadataService.java
@@ -0,0 +1,44 @@
+package org.mycore.jspdocportal.ir.pi;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jdom2.Element;
+import org.mycore.common.MCRConstants;
+import org.mycore.datamodel.metadata.MCRBase;
+import org.mycore.datamodel.metadata.MCRObject;
+import org.mycore.mods.MCRMODSWrapper;
+import org.mycore.mods.identifier.MCRAbstractMODSMetadataService;
+import org.mycore.pi.MCRPersistentIdentifier;
+import org.mycore.pi.exceptions.MCRPersistentIdentifierException;
+
+public class MCRMODSIdentifierMetadataService extends MCRAbstractMODSMetadataService {
+    private Logger LOGGER = LogManager.getLogger();
+    public MCRMODSIdentifierMetadataService(String inscriberID) {
+        super(inscriberID);
+    }
+
+    @Override
+    public void insertIdentifier(MCRPersistentIdentifier identifier, MCRBase base, String additional)
+        throws MCRPersistentIdentifierException {
+        MCRObject object = checkObject(base);
+        MCRMODSWrapper wrapper = new MCRMODSWrapper(object);
+
+        Element identifierElement = wrapper.getElement(getXPath());
+        final String type = getIdentifierType();
+
+        if (identifierElement != null) {
+            if (!identifier.asString().equals(identifierElement.getText())) {
+                LOGGER.warn(
+                    type + " with prefix " + getProperties().get(PREFIX_PROPERTY_KEY) + " already exist with value "
+                        + identifierElement.getText() + "! - It will be replaced with " + identifier.asString());
+                identifierElement.setText(identifier.asString());
+            }
+        }
+        else {
+            identifierElement = new Element("identifier", MCRConstants.MODS_NAMESPACE);
+            identifierElement.setAttribute("type", type);
+            identifierElement.setText(identifier.asString());
+            wrapper.addElement(identifierElement);
+        }
+    }
+}
