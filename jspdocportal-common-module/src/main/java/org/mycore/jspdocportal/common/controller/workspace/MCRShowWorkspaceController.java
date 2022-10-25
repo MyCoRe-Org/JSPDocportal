0f4fcfbe01423e0c7e1212196ae0d684dc41ec11
Robert Stephan <robert.stephan@uni-rostock.de>
2022-10-25 19:15:49 +0200

    use StringUtils from commons.lang3

--- jspdocportal-common-module/src/main/java/org/mycore/jspdocportal/common/controller/workspace/MCRShowWorkspaceController.java
+++ jspdocportal-common-module/src/main/java/org/mycore/jspdocportal/common/controller/workspace/MCRShowWorkspaceController.java
@@ -15,7 +15,7 @@ import java.util.TreeSet;
 
 import javax.xml.transform.TransformerFactory;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.camunda.bpm.engine.RuntimeService;
--- jspdocportal-ir-module/src/main/java/org/mycore/jspdocportal/ir/tileserver/MCRJSPTileFileProvider.java
+++ jspdocportal-ir-module/src/main/java/org/mycore/jspdocportal/ir/tileserver/MCRJSPTileFileProvider.java
@@ -27,7 +27,7 @@ import java.net.URLDecoder;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.mycore.common.config.MCRConfiguration2;
