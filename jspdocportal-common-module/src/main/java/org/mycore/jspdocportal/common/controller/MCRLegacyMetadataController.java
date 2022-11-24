0a168ea7501ce36650666bb841b251c85ab7c6fb
Robert Stephan <robert.stephan@uni-rostock.de>
2022-11-24 14:49:43 +0100

    validate MCRObjectID in MCRLegacyMetadataController

--- jspdocportal-common-module/src/main/java/org/mycore/jspdocportal/common/controller/MCRLegacyMetadataController.java
+++ jspdocportal-common-module/src/main/java/org/mycore/jspdocportal/common/controller/MCRLegacyMetadataController.java
@@ -2,12 +2,15 @@ package org.mycore.jspdocportal.common.controller;
 
 import java.net.URI;
 
+import org.mycore.datamodel.metadata.MCRObjectID;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 
 /**
  * Action Bean for deprecated /metadata URLs * 
@@ -20,6 +23,10 @@ public class MCRLegacyMetadataController {
     @GET
     public Response defaultRes(@PathParam("mcrid") String mcrid,
         @Context HttpServletRequest request) {
-        return Response.temporaryRedirect(URI.create(request.getContextPath() + "/resolve/id/" + mcrid)).build();
+        if (MCRObjectID.isValid(mcrid)) {
+            return Response.temporaryRedirect(URI.create(request.getContextPath() + "/resolve/id/" + mcrid)).build();
+        } else {
+            return Response.status(Status.NOT_FOUND).build();
+        }
     }
 }
