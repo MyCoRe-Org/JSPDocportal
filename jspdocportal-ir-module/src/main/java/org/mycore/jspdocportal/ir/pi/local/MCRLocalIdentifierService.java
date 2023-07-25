acbb54db40a1d855298164319fdb671cf9a8424c
Robert Stephan <robert.stephan@uni-rostock.de>
2023-07-25 17:53:35 +0200

    use new PI API

--- jspdocportal-ir-module/src/main/java/org/mycore/jspdocportal/ir/pi/local/MCRLocalIdentifierService.java
+++ jspdocportal-ir-module/src/main/java/org/mycore/jspdocportal/ir/pi/local/MCRLocalIdentifierService.java
@@ -1,10 +1,8 @@
 package org.mycore.jspdocportal.ir.pi.local;
 
-import java.util.Optional;
-
 import org.mycore.datamodel.metadata.MCRBase;
 import org.mycore.pi.MCRPIService;
-import org.mycore.pi.MCRPersistentIdentifier;
+import org.mycore.pi.MCRPIServiceDates;
 import org.mycore.pi.exceptions.MCRPersistentIdentifierException;
 
 public class MCRLocalIdentifierService extends MCRPIService<MCRLocalID> {
@@ -14,18 +12,7 @@ public class MCRLocalIdentifierService extends MCRPIService<MCRLocalID> {
         super(MCRLocalID.TYPE);
     }
 
-    @Override
-    protected void registerIdentifier(MCRBase obj, String additional, MCRLocalID pi)
-        throws MCRPersistentIdentifierException {
-        Optional<MCRPersistentIdentifier> oPI = getMetadataService().getIdentifier(obj, additional);
-        if (oPI.isPresent() && oPI.get() instanceof MCRLocalID) {
-            //do nothing
-        } else {
-            getNewIdentifier(obj, additional);
-        }
-    }
-
-    @Override
+   @Override
     protected void update(MCRLocalID identifier, MCRBase obj, String additional)
         throws MCRPersistentIdentifierException {
         //do nothing
@@ -37,6 +24,13 @@ public class MCRLocalIdentifierService extends MCRPIService<MCRLocalID> {
         throws MCRPersistentIdentifierException {
         // do nothing
 
+    }
+
+    @Override
+    protected MCRPIServiceDates registerIdentifier(MCRBase obj, String additional, MCRLocalID pi)
+        throws MCRPersistentIdentifierException {
+        //do nothing
+        return new MCRPIServiceDates(null, null);
     }
 
 }
