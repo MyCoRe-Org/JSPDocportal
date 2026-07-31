127615051c1828856bfaee0970f4c5fe28f19d8d
Lennard Golsch <lennard.golsch@icloud.com>
2026-07-31 15:46:46 +0200

    feat: add support for xslImports in transformXSL tag

--- jspdocportal-common-module/src/main/java/org/mycore/jspdocportal/common/taglibs/MCRTransformXslTag.java
+++ jspdocportal-common-module/src/main/java/org/mycore/jspdocportal/common/taglibs/MCRTransformXslTag.java
@@ -24,19 +24,37 @@ package org.mycore.jspdocportal.common.taglibs;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.StringReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.Templates;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXSource;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.mycore.common.MCRCache;
 import org.mycore.common.MCRClassTools;
+import org.mycore.common.MCRException;
+import org.mycore.common.config.MCRConfiguration2;
 import org.mycore.common.content.MCRDOMContent;
 import org.mycore.common.content.MCRJDOMContent;
 import org.mycore.common.content.transformer.MCRXSLTransformer;
+import org.mycore.common.xml.MCRXMLResource;
+import org.mycore.common.xsl.MCRTemplatesSource;
 import org.mycore.datamodel.common.MCRXMLMetadataManager;
 import org.mycore.datamodel.metadata.MCRObjectID;
 import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 
 import jakarta.servlet.jsp.JspException;
 import jakarta.servlet.jsp.tagext.SimpleTagSupport;
@@ -54,6 +72,7 @@ import jakarta.servlet.jsp.tagext.SimpleTagSupport;
 /* Example:
  * <mcr:retrieveObject mcrid="${mcrid}" varDOM="doc" />
  * <mcr:transformXSL dom="${doc}" xslt="xsl/xsl3example.xsl" />
+ * <mcr:transformXSL dom="${doc}" xslImports="docdetails-metadata" />
  */
 public class MCRTransformXslTag extends SimpleTagSupport {
     private static final Logger LOGGER = LogManager.getLogger();
@@ -66,15 +85,27 @@ public class MCRTransformXslTag extends SimpleTagSupport {
 
     private String mcrid;
 
+    private String xslImports;
+
     @Override
     public void doTag() throws JspException, IOException {
         try {
+            if (xslImports != null && stylesheet != null) {
+                throw new JspException("Attributes 'xslt' and 'xslImports' are mutually exclusive");
+            }
+
             // this works, if the default transformer is xslt3 (set by property):
             // MCR.LayoutService.TransformerFactoryClass=net.sf.saxon.TransformerFactoryImpl
             // MCRXSLTransformer t = MCRXSLTransformer.getInstance(stylesheet);
-
             Class<? extends TransformerFactory> tfClass = MCRClassTools.forName("net.sf.saxon.TransformerFactoryImpl");
-            MCRXSLTransformer t = MCRXSLTransformer.obtainInstance(tfClass, stylesheet);
+            MCRXSLTransformer t;
+            if (xslImports != null) {
+                String virtualStylesheet = createVirtualStylesheet(xslImports);
+                MCRTemplatesSource source = new MCRVirtualTemplatesSource(xslImports, virtualStylesheet);
+                t = new MCRTemplatesSourceXSLTransformer(tfClass, source);
+            } else {
+                t = MCRXSLTransformer.obtainInstance(tfClass, stylesheet);
+            }
 
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             if (mcrid != null) {
@@ -96,6 +127,10 @@ public class MCRTransformXslTag extends SimpleTagSupport {
         }
     }
 
+    public void setXslImports(String xslImports) {
+        this.xslImports = xslImports;
+    }
+
     public String getXslt() {
         return stylesheet;
     }
@@ -114,5 +149,85 @@ public class MCRTransformXslTag extends SimpleTagSupport {
 
     public void setJdom(org.jdom2.Document jdom) {
         this.jdom = jdom;
+    }
+
+    static class MCRTemplatesSourceXSLTransformer extends MCRXSLTransformer {
+
+        public MCRTemplatesSourceXSLTransformer(Class<? extends TransformerFactory> factoryClass,
+            MCRTemplatesSource... templateSources) {
+            super(factoryClass);
+            this.templateSources = templateSources;
+            this.modified = new long[this.templateSources.length];
+            this.modifiedChecked = 0L;
+            this.templates = new Templates[this.templateSources.length];
+        }
+    }
+
+    public static String createVirtualStylesheet(String importName) {
+        return """
+        <?xml version="1.0" encoding="UTF-8"?>
+        <xsl:stylesheet version="3.0"
+            xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+            <xsl:import href="xslImport:%s"/>
+            <xsl:output method="html" indent="yes" standalone="no" encoding="UTF-8" />
+
+        </xsl:stylesheet>
+        """.formatted(importName);
+    }
+
+    static class MCRVirtualTemplatesSource extends MCRTemplatesSource {
+
+        private final String name;
+        private final String content;
+        private final Long created;
+
+        public MCRVirtualTemplatesSource(String name, String content) {
+            super("virtual_" + name);
+            this.name = name;
+            this.content = content;
+            this.created = System.currentTimeMillis();
+        }
+
+        @Override
+        public SAXSource getSource() throws SAXException, ParserConfigurationException {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+
+            XMLReader reader = factory.newSAXParser().getXMLReader();
+            InputSource inputSource = new InputSource(new StringReader(content));
+            inputSource.setSystemId(getURL().toExternalForm());
+
+            return new SAXSource(reader, inputSource);
+        }
+
+        @Override
+        public URL getURL() {
+            try {
+                return URI.create("file:/virtual/" + name + ".xsl").toURL();
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public long getLastModified() {
+            return created;
+        }
+
+        @Override
+        public MCRCache.ModifiedHandle getModifiedHandle(long checkPeriod) {
+            return new MCRCache.ModifiedHandle() {
+                @Override
+                public long getLastModified() {
+                    return created;
+                }
+
+                @Override
+                public long getCheckPeriod() {
+                    return checkPeriod;
+                }
+            };
+        }
     }
 }
